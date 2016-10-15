949b0446e955799186eee9d36768608f02dc92b8
david <david@akademia.co.jp>
2016-10-15 17:41:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+NodeJS server
+
+1. clone project (git clone https://github.com/vessp/node-starter.git)
+2. cd into project folder
+3. npm install
+4. npm start (to run server)
---
+++ server.js
@@ -0,0 +1,59 @@
+'use strict';
+
+//Imports------------------------------------------------------------------------
+const express = require('express');
+const path = require('path');
+const SocketServer = require('ws').Server;
+
+
+//Server-------------------------------------------------------------------------
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+app.route('/')
+    .get((request, response) => {
+        response.sendFile(INDEX)
+    })
+
+const server = app.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`)
+});
+
+
+//Web Socket---------------------------------------------------------------------
+const wss = new SocketServer({ server });
+var userCount = 0
+
+wss.on('connection', (ws) => {
+    console.log('onConnection');
+    userCount++
+    yellUserCount()
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+        userCount--
+        yellUserCount()
+    });
+
+    ws.on('message', message => {
+        console.log('onMessage: ', message)
+        //{type: <TYPE>, payload: <MESSAGE>}
+        let json = JSON.parse(message)
+        yellMessage(message)
+    });
+});
+
+function yellMessage(message) {
+    wss.clients.forEach(ws => {
+        ws.send(JSON.stringify(message))
+    });
+}
+
+function yellUserCount() {
+    yellMessage({
+        'type':'userCount',
+        'message': userCount
+    })
+}
